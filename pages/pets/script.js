839b30bbba44c5373ff344369457ed35febcd458
Nargiza Akunova <nargizaakun@gmail.com>
2022-05-02 14:21:20 +0200

    feat: add hamburger and js for it, refactor responsive layout of Pets page

---
+++ pages/pets/script.js
@@ -0,0 +1,20 @@
+// Hamburger
+
+document.querySelector(".hamburger").addEventListener("click", function (e) {
+  console.log(this);
+  this.classList.toggle("open");
+  e.stopPropagation();
+});
+
+document.querySelector(".hamburger").addEventListener("click", function (e) {
+  console.log(this);
+  document.querySelector(".sidebar-menu").classList.toggle("open-menu");
+  e.stopPropagation();
+});
+
+// Close sidebar when clicking out of the links
+
+document.addEventListener("click", function () {
+  document.querySelector(".hamburger").classList.toggle("open");
+  document.querySelector(".sidebar-menu").classList.toggle("open-menu");
+});
