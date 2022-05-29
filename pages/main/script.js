42af54a2eb7b1d86f93211e7d0700325849f59c8
Nargiza Akunova <nargizaakun@gmail.com>
2022-05-29 22:46:52 +0200

    feat: copy hamburger and sidenav from pets to main

--- pages/main/script.js
+++ pages/main/script.js
@@ -3,3 +3,24 @@
 function visitPage() {
   window.location = "../pets/pets.html";
 }
+
+// Hamburger
+
+document.querySelector(".hamburger").addEventListener("click", function (e) {
+  console.log(this);
+  this.classList.toggle("open");
+  e.stopPropagation();
+});
+
+document.querySelector(".hamburger").addEventListener("click", function (e) {
+  console.log(this);
+  document.querySelector(".sidebar-menu").classList.toggle("open-menu");
+  e.stopPropagation();
+});
+
+// Close sidebar when clicking out of the links
+
+document.addEventListener("click", function () {
+  document.querySelector(".hamburger").classList.toggle("open");
+  document.querySelector(".sidebar-menu").classList.toggle("open-menu");
+});
