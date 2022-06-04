8beba87b85bcc7908cc9570a4c3d3bd627eec74d
Nargiza Akunova <nargizaakun@gmail.com>
2022-06-04 21:07:56 +0200

    feat: modal windows open with overlay on main page

--- pages/main/script.js
+++ pages/main/script.js
@@ -8,8 +8,7 @@ function visitPage() {
 
 document.querySelector(".hamburger").addEventListener("click", function (e) {
   // console.log(this);
-  this.classList.toggle("open");
-  document.querySelector(".sidebar-menu").classList.toggle("open-menu");
+  openMenu();
 
   const isMenuOpen = this.classList.contains("open");
   if (isMenuOpen) {
@@ -19,14 +18,6 @@ document.querySelector(".hamburger").addEventListener("click", function (e) {
   }
 
   e.stopPropagation();
-});
-
-// Close sidebar when clicking out of the links
-
-document.addEventListener("click", function () {
-  document.querySelector(".hamburger").classList.remove("open");
-  document.querySelector(".sidebar-menu").classList.remove("open-menu");
-  hideOverlay();
 });
 
 let page = 0;
@@ -76,7 +67,11 @@ function createSliderCard(pet) {
   const learnMoreBtnEl = document.createElement("button");
   learnMoreBtnEl.classList.add("learn-more-btn");
   learnMoreBtnEl.innerText = "Learn more";
-  learnMoreBtnEl.onclick = openModalWindow();
+  learnMoreBtnEl.onclick = () => {
+    console.log("open");
+    openModalWindow(pet);
+    showOverlay();
+  };
 
   sliderCardEl.append(imgEl, petNameEl, learnMoreBtnEl);
 
@@ -89,6 +84,12 @@ function showOverlay() {
   overlayEl.classList.add("overlay");
   document.body.appendChild(overlayEl);
   document.documentElement.classList.add("no-scroll");
+
+  overlayEl.onclick = function () {
+    hideMenu();
+    hideOverlay();
+    closeModalWindow();
+  };
 }
 
 function hideOverlay() {
@@ -100,9 +101,88 @@ function hideOverlay() {
   document.documentElement.classList.remove("no-scroll");
 }
 
+function openMenu() {
+  this.classList.toggle("open");
+  document.querySelector(".sidebar-menu").classList.toggle("open-menu");
+}
+
+function hideMenu() {
+  document.querySelector(".hamburger").classList.remove("open");
+  document.querySelector(".sidebar-menu").classList.remove("open-menu");
+}
+
 // Modal windows
-function openModalWindow() {}
-function closeModalWindow() {}
+function createModalWindow(pet) {
+  const modalEl = document.createElement("div");
+  modalEl.classList.add("modal");
+
+  const closeBtnEl = document.createElement("button");
+  closeBtnEl.classList.add("close-modal-btn");
+  closeBtnEl.onclick = () => {
+    closeModalWindow();
+    hideOverlay();
+  };
+
+  const vectorCloseImgEl = document.createElement("img");
+  vectorCloseImgEl.src = "../../assets/icons/Vector1.svg";
+  vectorCloseImgEl.alt = "Close modal window button";
+
+  const modalBodyEl = document.createElement("div");
+  modalBodyEl.classList.add("modal-body");
+
+  const petImageEl = document.createElement("img");
+  petImageEl.classList.add("pet-image");
+  petImageEl.src = pet.img;
+  petImageEl.alt = "Pet";
+
+  const petCharacterEl = document.createElement("div");
+  petCharacterEl.classList.add("pet-character");
+
+  const block1El = document.createElement("div");
+  const petNameEl = document.createElement("h3");
+  petNameEl.classList.add("pet-name");
+  petNameEl.innerText = pet.name;
+  const petTypeBreedEl = document.createElement("h4");
+  petTypeBreedEl.classList.add("pet-breed");
+  petTypeBreedEl.innerText = `${pet.type} - ${pet.breed}`;
+
+  const block2El = document.createElement("div");
+  const petDescriptionEl = document.createElement("p");
+  petDescriptionEl.classList.add("pet-description");
+  petDescriptionEl.innerText = pet.description;
+
+  const block3El = document.createElement("div");
+  const petListEl = document.createElement("ul");
+  petListEl.classList.add("pet-list");
+  const petAgeEl = document.createElement("li");
+  petAgeEl.innerHTML = `<span><strong>Age: </strong>${pet.age}</span>`;
+  const petInoculationsEl = document.createElement("li");
+  petInoculationsEl.innerHTML = `<span><strong>Inoculations: </strong>${pet.inoculations}</span>`;
+  const petDiseasesEl = document.createElement("li");
+  petDiseasesEl.innerHTML = `<span><strong>Diseases: </strong>${pet.diseases}</span>`;
+  const petParasitesEl = document.createElement("li");
+  petParasitesEl.innerHTML = `<span><strong>Parasites: </strong>${pet.parasites}</span>`;
+
+  petListEl.append(petAgeEl, petInoculationsEl, petDiseasesEl, petParasitesEl);
+  block1El.append(petNameEl, petTypeBreedEl);
+  block2El.append(petDescriptionEl);
+  block3El.append(petListEl);
+  petCharacterEl.append(block1El, block2El, block3El);
+  modalBodyEl.append(petImageEl, petCharacterEl);
+  closeBtnEl.append(vectorCloseImgEl);
+  modalEl.append(closeBtnEl, modalBodyEl);
+  return modalEl;
+}
+
+function openModalWindow(pet) {
+  document.body.append(createModalWindow(pet));
+}
+function closeModalWindow() {
+  const modalWindowEl = document.querySelector(".modal");
+  if (modalWindowEl) {
+    modalWindowEl.remove();
+  }
+}
 // Pagination
 
 // Fetch json file
