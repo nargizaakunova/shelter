826f485ad6c537e9222c14a9c27501e06f346c17
Nargiza Akunova <nargizaakun@gmail.com>
2022-06-04 16:23:37 +0200

    feat: add overlay on both pages

--- pages/main/script.js
+++ pages/main/script.js
@@ -7,14 +7,17 @@ function visitPage() {
 // Hamburger
 
 document.querySelector(".hamburger").addEventListener("click", function (e) {
-  console.log(this);
+  // console.log(this);
   this.classList.toggle("open");
-  e.stopPropagation();
-});
-
-document.querySelector(".hamburger").addEventListener("click", function (e) {
-  console.log(this);
   document.querySelector(".sidebar-menu").classList.toggle("open-menu");
+
+  const isMenuOpen = this.classList.contains("open");
+  if (isMenuOpen) {
+    showOverlay();
+  } else {
+    hideOverlay();
+  }
+
   e.stopPropagation();
 });
 
@@ -23,6 +26,7 @@ document.querySelector(".hamburger").addEventListener("click", function (e) {
 document.addEventListener("click", function () {
   document.querySelector(".hamburger").classList.remove("open");
   document.querySelector(".sidebar-menu").classList.remove("open-menu");
+  hideOverlay();
 });
 
 let page = 0;
@@ -46,8 +50,74 @@ btnRightEl.addEventListener("click", function (e) {
   slideTo(page);
 });
 
+function createSliderCard(pet) {
+  // {
+  //   "name": "Jennifer",
+  //   "img": "../../assets/images/jennifer.png",
+  //   "type": "Dog",
+  //   "breed": "Labrador",
+  //   "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
+  //   "age": "2 months",
+  //   "inoculations": ["none"],
+  //   "diseases": ["none"],
+  //   "parasites": ["none"]
+  // },
+  const sliderCardEl = document.createElement("div");
+  sliderCardEl.classList.add("slider-card");
+
+  const imgEl = document.createElement("img");
+  imgEl.classList.add("pet-image");
+  imgEl.src = pet.img;
+
+  const petNameEl = document.createElement("p");
+  petNameEl.classList.add("pet-name");
+  petNameEl.innerText = pet.name;
+
+  const learnMoreBtnEl = document.createElement("button");
+  learnMoreBtnEl.classList.add("learn-more-btn");
+  learnMoreBtnEl.innerText = "Learn more";
+  learnMoreBtnEl.onclick = openModalWindow();
+
+  sliderCardEl.append(imgEl, petNameEl, learnMoreBtnEl);
+
+  return sliderCardEl;
+}
+
 // Dark overlay when opening modal windows and nav-sidebar
+function showOverlay() {
+  const overlayEl = document.createElement("div");
+  overlayEl.classList.add("overlay");
+  document.body.appendChild(overlayEl);
+  document.documentElement.classList.add("no-scroll");
+}
+
+function hideOverlay() {
+  const overlayEl = document.querySelector(".overlay");
+  console.log(overlayEl);
+  if (overlayEl) {
+    overlayEl.remove();
+  }
+  document.documentElement.classList.remove("no-scroll");
+}
 
 // Modal windows
-
+function openModalWindow() {}
+function closeModalWindow() {}
 // Pagination
+
+// Fetch json file
+
+fetch("../../assets/pets.json")
+  .then((response) => response.json())
+  .then((pets) => {
+    const fragment = new DocumentFragment();
+
+    pets.forEach((pet) => {
+      const sliderCard = createSliderCard(pet);
+      fragment.append(sliderCard);
+    });
+    const sliderEl = document.getElementsByClassName("slider");
+    if (sliderEl.length > 0) {
+      sliderEl[0].append(fragment);
+    }
+  });
--- pages/pets/script.js
+++ pages/pets/script.js
@@ -1,14 +1,16 @@
 // Hamburger
 
 document.querySelector(".hamburger").addEventListener("click", function (e) {
-  console.log(this);
+  // console.log(this);
   this.classList.toggle("open");
-  e.stopPropagation();
-});
-
-document.querySelector(".hamburger").addEventListener("click", function (e) {
-  console.log(this);
   document.querySelector(".sidebar-menu").classList.toggle("open-menu");
+
+  const isMenuOpen = this.classList.contains("open");
+  if (isMenuOpen) {
+    showOverlay();
+  } else {
+    hideOverlay();
+  }
   e.stopPropagation();
 });
 
@@ -17,4 +19,28 @@ document.querySelector(".hamburger").addEventListener("click", function (e) {
 document.addEventListener("click", function () {
   document.querySelector(".hamburger").classList.remove("open");
   document.querySelector(".sidebar-menu").classList.remove("open-menu");
+  hideOverlay();
 });
+
+// Dark overlay when opening modal windows and nav-sidebar
+function showOverlay() {
+  const overlayEl = document.createElement("div");
+  overlayEl.classList.add("overlay");
+  document.body.appendChild(overlayEl);
+  document.querySelector("header").style.position = "absolute";
+  document.querySelector("header").style.zIndex = "unset";
+  document.querySelector(".sidebar-menu").style.position = "fixed";
+  document.documentElement.classList.add("no-scroll");
+}
+
+function hideOverlay() {
+  const overlayEl = document.querySelector(".overlay");
+  // console.log(overlayEl);
+  if (overlayEl) {
+    overlayEl.remove();
+  }
+  document.querySelector("header").style.position = "fixed";
+  document.querySelector("header").style.zIndex = "1";
+  document.querySelector(".sidebar-menu").style.position = "absolute";
+  document.documentElement.classList.remove("no-scroll");
+}
